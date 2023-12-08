c49e83f795d264fd94269518434965a7cc15e342
Arghya Das <github@movevirtual.co>
2023-12-08 19:39:58 +0530

    new askdocs component sheet close menu color red with box shadow

--- app/chat/component/sidebarcomponent.tsx
+++ app/chat/component/sidebarcomponent.tsx
@@ -7,12 +7,12 @@ export default function SidebarComponent() {
   return (
     <div className="sidebar__component p-5 flex flex-col justify-between h-full">
       <div className="sidebar__upper--component">
-        <ProfileComponent />
         <UploadButton />
         <DocList />
       </div>
       <div className="sidebar__lower__component">
         <CreditComponent />
+        <ProfileComponent />
       </div>
     </div>
   );
--- app/chat/sidebar.tsx
+++ app/chat/sidebar.tsx
@@ -1,12 +1,8 @@
-import ProfileComponent from "./component/profilecomponent";
-import UploadButton from "./component/uploadbutton";
-import DocList from "./component/doclist";
-import CreditComponent from "./component/creditcomponent";
 import SidebarComponent from "./component/sidebarcomponent";
 
 export default function Sidebar() {
   return (
-    <section className="chat__sidebar w-[300px] hidden xl:block border border-dashed h-screen">
+    <section className="chat__sidebar xl:w-[300px] hidden xl:block border border-dashed h-screen">
       <SidebarComponent />
     </section>
   );
