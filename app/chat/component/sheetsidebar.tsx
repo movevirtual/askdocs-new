60491c7aa31622fee94dc7449348fb5b75ecd699
Arghya Das <github@movevirtual.co>
2023-12-08 12:16:08 +0530

    new askdocs component sheetsideview

--- app/chat/component/sheetsidebar.tsx
+++ app/chat/component/sheetsidebar.tsx
@@ -17,7 +17,7 @@ export default function SheetSidebar() {
         <SheetTrigger>
           <IoSettingsOutline className="xl:hidden absolute top-2 right-2 text-xl text-slate-500" />
         </SheetTrigger>
-        <SheetContent side={"left"} className="w-[300px] p-0 pt-5">
+        <SheetContent side={"left"} className="w-[300px] p-0">
           <SidebarComponent />
         </SheetContent>
       </Sheet>
